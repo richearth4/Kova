370c0f3455b6f8adcc22ff0aaa80f2fc082b7aba
Richie <richie.udoh@gmail.com>
2026-08-14 22:10:15 +0100

    fix: Resolve build errors by casting window type and building with webpack

--- src/components/RegisterSW.tsx
+++ src/components/RegisterSW.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 export function RegisterSW() {
   useEffect(() => {
-    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox === undefined) {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox === undefined) {
       window.addEventListener('load', () => {
         navigator.serviceWorker
           .register('/sw.js')
